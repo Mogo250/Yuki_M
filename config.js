0c4d779e12f56aded4c5ab1e42302bafb686b170
Mogo250 <mogtba250@gmail.com>
2024-08-17 19:35:49 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,115 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+
+//
+global.available = true;
+global.autoReadAll = false;
+global.antitags = false;
+
+
+//auto functioner
+global.autoTyping = false;                //auto tying by default off.
+global.autoRecord = false;                //auto recording by default off.
+global.groupevent = false;                //This is the new variable for controlling group event handling.
+global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.autoreadgc = false;
+
+
+//
+global.prefa = ["-"];                                    //Default prefix here. you can change if you want.
+
+
+//
+global.Owner = ['249995436584' , "249992134574"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ['249995436584' , "2499921345574"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ['249995436584' , "249992134574"];
+global.yukio = ["249991672637"];
+global.OwnerName = "TRAMAZOOL";
+global.BotName = "يوكي";
+global.packname = "Yuki Bot";                             //Do not change.
+global.author = "By:Trama";                               //Do not change.
+global.BotSourceCode = "https://github.com/Trama-007/A17"; //Do not change.
+global.SupportGroupLink = "https://chat.GOOGLE.COM";
+
+
+//
+global.sessionName = "session";                          //Do not change.
+
+
+//
+global.openAiAPI = "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";
+
+
+//
+global.location = "Shendi, sudan";
+global.reactmoji = "❤️";
+global.themeemoji = "💖";
+global.vidmenu = { url: 'https://graph.org/file/b52c1fc846c9951033a93.jpg' };
+global.websitex = "https://github.com/TRAMA-007";
+global.lolhuman = "KaysaS";
+
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+//
+global.mess = {
+  jobdone: 'استلم يا مكتب...',
+  useradmin: 'أسفة فقط *الادمنز* يمكنهم استخدام هذا الأمر!',
+  botadmin: 'المعذرة، يجب أن أكون *ادمن* لتنفيذ هذا الأمر.',
+  botowner: 'فقط *تراما* من يمكنه استخدام هذا الأمر! !',
+  grouponly: 'هذا الأمر يعمل فقط في *المجموعات*،!',
+  privateonly: 'This command is only made for *Private Chat*, Baka!',
+  botonly: 'Only the *Bot itself* can use this command!',
+  waiting: 'انتظر من فضلك...',
+  nolink: 'آنو..., ، شكلك نسيت تكتب الرابط؟',
+  error: 'خطأ في العملية ، الرجاء المحاولة لاحقاً',
+  banned: 'آسفة ، بس انت محظور من البوت',
+  bangc: 'آسفة بس القروب دا محظور من البوت',
+  nonsfw: 'أمنع الفارغة يا زول!'
+
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
